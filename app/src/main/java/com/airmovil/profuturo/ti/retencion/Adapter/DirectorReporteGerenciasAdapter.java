acf0d96ed559c585b5c64b618968f89cc67b7c72
tecnico airmovil <tecnicoairmovil@INFINITUMAR43GR.local>
2017-04-20 13:41:29 -0500

    Director gerencias

--- app/src/main/java/com/airmovil/profuturo/ti/retencion/Adapter/DirectorReporteGerenciasAdapter.java
+++ app/src/main/java/com/airmovil/profuturo/ti/retencion/Adapter/DirectorReporteGerenciasAdapter.java
@@ -34,6 +34,7 @@ import com.airmovil.profuturo.ti.retencion.directorFragmento.ReporteSucursales;
 import com.airmovil.profuturo.ti.retencion.helper.Config;
 import com.airmovil.profuturo.ti.retencion.helper.Connected;
 
+import com.airmovil.profuturo.ti.retencion.helper.EnviaMail;
 import com.airmovil.profuturo.ti.retencion.listener.OnLoadMoreListener;
 import com.airmovil.profuturo.ti.retencion.model.DirectorReporteGerenciasModel;
 
@@ -42,6 +43,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.airmovil.profuturo.ti.retencion.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by cesarriver on 31/03/17.
@@ -227,7 +231,7 @@ public class DirectorReporteGerenciasAdapter extends RecyclerView.Adapter {
                     dialog.setContentView(R.layout.custom_layout);
 
                     Button btn = (Button) dialog.findViewById(R.id.dialog_btn_enviar);
-                    Spinner spinner = (Spinner) dialog.findViewById(R.id.dialog_spinner_mail);
+                    final Spinner spinner = (Spinner) dialog.findViewById(R.id.dialog_spinner_mail);
 
                     // TODO: Spinner
                     ArrayAdapter<String> adapterSucursal = new ArrayAdapter<String>(mContext, R.layout.spinner_item_azul, Config.EMAIL);
@@ -237,22 +241,71 @@ public class DirectorReporteGerenciasAdapter extends RecyclerView.Adapter {
                     btn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            EditText editText = (EditText) dialog.findViewById(R.id.dialog_et_mail);
+                            final EditText editText = (EditText) dialog.findViewById(R.id.dialog_et_mail);
 
                             final String datoEditText = editText.getText().toString();
-                            Connected connected = new Connected();
-                            if(connected.estaConectado(mContext)){
+                            final String datoSpinner = spinner.getSelectedItem().toString();
 
-                                InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Service.INPUT_METHOD_SERVICE);
-                                imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-                                Config.msjTime(mContext, "Enviando", "Se ha enviado el mensaje al destino", 4000);
-                                dialog.dismiss();
+                            Log.d("DATOS USER","SPINNER: "+datoEditText+" datosSpinner: "+ datoSpinner);
+                            if(datoEditText == "" || datoSpinner == "Seleciona un email"){
+                                Config.msj(mContext, "Error", "Ingresa email valido");
                             }else{
-                                Config.msj(mContext,"Error conexión", "Por favor, revisa tu conexión a internet");
-                                dialog.dismiss();
-                            }
-                            //final String datoSpinner = spinner.getSelectedItem().toString()
+                                String email = datoEditText+"@"+datoSpinner;
+                                Connected connected = new Connected();
+                                final InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Service.INPUT_METHOD_SERVICE);
+                                if(connected.estaConectado(mContext)){
+                                    JSONObject obj = new JSONObject();
+                                    try {
+                                        JSONObject rqt = new JSONObject();
+                                        rqt.put("correo", email);
+                                        rqt.put("detalle", true);
+                                        rqt.put("idGerencia", list.getIdGerencia());
+                                        JSONObject periodo = new JSONObject();
+                                        periodo.put("fechaFin", mFechaFin);
+                                        periodo.put("fechaInicio", mFechaInicio);
+                                        rqt.put("periodo", periodo);
+                                        rqt.put("usuario", Config.usuarioCusp(mRecyclerView.getContext()));
+                                        obj.put("rqt", rqt);
+                                        Log.d("datos", "REQUEST-->" + obj);
+                                    } catch (JSONException e) {
+                                        Config.msj(mContext, "Error", "Error al formar los datos");
+                                    }
+                                    EnviaMail.sendMail(obj,Config.URL_SEND_MAIL_REPORTE_GERENCIA,mContext,new EnviaMail.VolleyCallback() {
 
+                                        @Override
+                                        public void onSuccess(JSONObject result) {
+                                            Log.d("RESPUESTA DIRECTOR", result.toString());
+                                            int status;
+
+                                            try {
+                                                status = result.getInt("status");
+                                            }catch(JSONException error){
+                                                status = 400;
+                                            }
+
+                                            Log.d("EST","EE: "+status);
+                                            imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                                            if(status == 200) {
+                                                Config.msj(mContext, "Enviando", "Se ha enviado el mensaje al destino");
+                                                //Config.msjTime(mContext, "Enviando", "Se ha enviado el mensaje al destino", 4000);
+                                                dialog.dismiss();
+                                            }else{
+                                                Config.msj(mContext, "Error", "Ups algo salio mal =(");
+                                                dialog.dismiss();
+                                            }
+                                            //db.addUserCredits(fk_id_usuario,result);
+                                        }
+                                        @Override
+                                        public void onError(String result) {
+                                            Log.d("RESPUESTA ERROR", result);
+                                            Config.msj(mContext, "Error en conexión", "Por favor, revisa tu conexión a internet ++");
+                                            //db.addUserCredits(fk_id_usuario, "ND");
+                                        }
+                                    });
+                                }else{
+                                    Config.msj(mContext, "Error en conexión", "Por favor, revisa tu conexión a internet");
+                                }
+                            }
                         }
                     });
                     dialog.show();
